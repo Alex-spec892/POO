e1917457d1ba49b3d3edb386ecd57266774ba3e7
Alex Alves <alexprof514@gmail.com>
2026-04-22 17:22:22 -0300

    aceita Strings como argumento

--- EncapsulamentoConstrutores/src/nome/Nomes.java
+++ EncapsulamentoConstrutores/src/nome/Nomes.java
@@ -3,8 +3,26 @@ package nome;
 public class Nomes {
     private String nomeProprio;
     private String nomeMeio;
+    private String nomeFamilia;
+    private String nomeCompleto;
+    private String rubrica;
 
-    
+    public Nomes(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+
+    }
+    public Nomes(String nomeProprio, String nomeMeio, String nomeFamilia) {
+        this.nomeProprio = nomeProprio;
+        this.nomeMeio = nomeMeio;
+        this.nomeFamilia = nomeFamilia;
+    }
+    public Nomes(){
+        this.nomeCompleto = "";
+    }
+
+    public String getNomeFamilia() {
+        return nomeFamilia;
+    }
 
     public void setNomeFamilia(String nomeFamilia) {
         this.nomeFamilia = nomeFamilia;
@@ -26,5 +44,28 @@ public class Nomes {
         this.nomeProprio = nomeProprio;
     }
 
-    private String nomeFamilia;
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    public void imprimirNomes(){
+        System.out.println("Nome completo: " + nomeCompleto);
+    }
+    public void imprimirNomes2(){
+        System.out.println("Nome completo2: " + nomeProprio + " " + nomeMeio + " " + nomeFamilia);
+    }
+    public void retornarRubrica(){
+        String[] partes = nomeCompleto.split(" ");
+        String rubrica = "";
+        System.out.println("Rubrica: " + rubrica);
+        for(String nome : partes){
+            if(!nome.isEmpty()){
+                rubrica += nome.charAt(0);
+            }
+        }
+        System.out.println("Rubrica: " + rubrica.toUpperCase());
+    }
 }
---
+++ EncapsulamentoConstrutores/src/nome/NomesMain.java
@@ -0,0 +1,18 @@
+package nome;
+
+import java.util.Scanner;
+
+public class NomesMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Nomes nome1 = new Nomes("Alex Alves Santos");
+        Nomes nome2 = new Nomes("João", "Pereira", "Silva");
+        Nomes nome3 = new Nomes();
+        nome3.setNomeCompleto(sc.nextLine());
+        nome1.imprimirNomes();
+        nome1.retornarRubrica();
+        nome2.imprimirNomes2();
+        nome3.imprimirNomes();
+
+    }
+}
